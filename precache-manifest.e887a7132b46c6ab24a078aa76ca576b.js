c898633a16a7b58f0572eb0e0a77f082c2eabecc
Nerbyk <danil.maximov2000@gmail.com>
2020-10-19 10:34:36 -0400

    Updates

---
+++ precache-manifest.e887a7132b46c6ab24a078aa76ca576b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57aadc1850c483415a03861763d6c9b",
+    "url": "/Web-Scraper-React/index.html"
+  },
+  {
+    "revision": "d3db3752d680b32a2d65",
+    "url": "/Web-Scraper-React/static/css/main.56679b15.chunk.css"
+  },
+  {
+    "revision": "8be7e38932ecd6858885",
+    "url": "/Web-Scraper-React/static/js/2.47994c19.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/Web-Scraper-React/static/js/2.47994c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3db3752d680b32a2d65",
+    "url": "/Web-Scraper-React/static/js/main.9fc9dba1.chunk.js"
+  },
+  {
+    "revision": "856030a3f3482150de03",
+    "url": "/Web-Scraper-React/static/js/runtime-main.95750849.js"
+  }
+]);
